03c8abfa3b17a73263c05ffc6ca17ef60fc4b106
Rabih-Achraf <76865754+Rabih-Achraf@users.noreply.github.com>
2021-01-30 13:36:43 +0100

    Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employer E1 = new Employer(2000.27, "Achraf", "Rabih");
+		Employer E2 = new Employer(4400.77, "Oussama", "Rabih");
+		
+		System.out.println("E1 : " + E1.nationalite);
+		System.out.println("E2 : " + E2.nationalite);
+		
+	}
+	
+}
